1ced740c36a326864730c1c9eb2eb28f390864fa
xenoriqlabs <xenoriqlabs@gmail.com>
2026-08-24 22:44:54 +0500

    Initial Commit

---
+++ Phase 1 — C++ Fundamentals/03-cpp-first-program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Display a message
+    cout << "Hello, World!\n";
+
+    // Display another message
+    cout << "I am learning C++.";
+
+    return 0;
+}
