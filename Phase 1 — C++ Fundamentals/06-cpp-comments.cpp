ee39656549e737828f48abed4bba7b11f115b9dd
xenoriqlabs <xenoriqlabs@gmail.com>
2026-08-24 23:58:02 +0500

    Initial Commit

---
+++ Phase 1 — C++ Fundamentals/06-cpp-comments.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // This is a single-line comment
+    cout << "Hello World\n";
+
+    /*
+        This is a multi-line comment.
+        It can span multiple lines.
+        The compiler ignores comments.
+    */
+    cout << "I am learning C++\n";
+
+
+    return 0;
+}
