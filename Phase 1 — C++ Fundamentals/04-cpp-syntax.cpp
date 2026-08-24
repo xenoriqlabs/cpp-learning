1f643801f5a1b6631540a7009275cb1dbcb747ee
xenoriqlabs <xenoriqlabs@gmail.com>
2026-08-24 23:50:57 +0500

    Initial Commit

--- Phase 1 — C++ Fundamentals/04-cpp-syntax.cpp
+++ Phase 1 — C++ Fundamentals/04-cpp-syntax.cpp
@@ -4,14 +4,17 @@ using namespace std;
 int main()
 {
     // Statements
+
     int age = 20;
     cout << "Age: " << age << "\n";
 
     // Case sensitivity
+
     int Age = 25;
     cout << "Age: " << Age << "\n";
 
     // Braces create a code block
+    
     if (age >= 18)
     {
         cout << "Adult\n";
