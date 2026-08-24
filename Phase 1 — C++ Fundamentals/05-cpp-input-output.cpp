855ed047e0e47cb311b1f5a95b515cf4aa3d0aec
xenoriqlabs <xenoriqlabs@gmail.com>
2026-08-24 23:48:56 +0500

    Initial Commit

---
+++ Phase 1 — C++ Fundamentals/05-cpp-input-output.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1. cout — Output
+    cout << "Hello World!\n";
+
+    // 2. cin — Input
+    int age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Your age is " << age << ".\n";
+
+    // 3. cerr (display error messages)
+    cerr << "This is an error message.\n";
+
+    // 4. clog — Log Output (generally logging/debugging information)
+    clog << "Program is loading...\n";
+
+    // 5. endl — New Line + Flush
+    cout << "Hello" << endl;
+    cout << "World" << endl;
+
+    // 6. \n — New Line
+    cout << "Line 1\n";
+    cout << "Line 2\n";
+    cout << "Line 3\n";
+}
