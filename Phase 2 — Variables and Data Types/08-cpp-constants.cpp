3b38f558445a91c0f7f71a360e60430dea4fb0f3
xenoriqlabs <xenoriqlabs@gmail.com>
2026-08-30 17:52:47 +0500

    Initial Commit

--- Phase 2 — Variables and Data Types/08-cpp-constants.cpp
+++ Phase 2 — Variables and Data Types/08-cpp-constants.cpp
@@ -2,6 +2,69 @@
 
 // In C++, you mainly use:
 
-    // const
-    // constexpr
+// const
+// constexpr
 
+#include <iostream>
+using namespace std;
+
+constexpr int square(int number)
+{
+    return number * number;
+}
+
+int main()
+{
+    // ========================================
+    // 1. const
+    // ========================================
+
+    const int age = 22;
+
+    cout << "Age: " << age << endl;
+
+    // age = 23;    // ❌ Error
+
+    // ========================================
+    // 2. Constant Variables
+    // ========================================
+
+    const double PI = 3.14159;
+    const double TAX_RATE = 0.18;
+
+    cout << "PI: " << PI << endl;
+    cout << "Tax Rate: " << TAX_RATE << endl;
+
+    // ========================================
+    // 3. constexpr
+    // ========================================
+
+    constexpr int DAYS_IN_WEEK = 7;
+    constexpr int MONTHS_IN_YEAR = 12;
+
+    cout << "Days in Week: " << DAYS_IN_WEEK << endl;
+    cout << "Months in Year: " << MONTHS_IN_YEAR << endl;
+
+    // ========================================
+    // 4. Practical Example
+    // ========================================
+
+    constexpr int MAX_SCORE = 100;
+
+    int studentScore = 85;
+
+    cout << "Student Score: " << studentScore << endl;
+    cout << "Maximum Score: " << MAX_SCORE << endl;
+
+    // MAX_SCORE = 90;  // ❌ Error
+
+    // ========================================
+    // 5. constexpr functions
+    // ========================================
+
+    constexpr int result = square(5);
+
+    cout << result;
+
+    return 0;
+}
